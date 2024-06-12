c8c1b9ba2267c2bb2b280db5eaaebabe14bf3aa7
DiCompanyTech <dicompanytech@gmail.com>
2024-06-12 10:38:15 -0300

    first commit

--- test/Escrow.js
+++ test/Escrow.js
@@ -7,4 +7,11 @@ const tokens = (n) => {
 
 describe('Escrow', () => {
 
+    it('Returns NFT Address' ,  async() => {
+        RealEstate = await ethers.getContractFactory('RealEstate')
+        RealEstate  = await RealEstate.deploy()
+
+        console.log(RealEstate.address)
+    })
+
 })
